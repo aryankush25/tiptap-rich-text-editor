3c88b6df2820db2546057c680aba58d0f280be70
Aryan Agarwal <aryankush025@gmail.com>
2024-04-18 21:54:36 +0530

    Add MantineProvider and import @mantine/tiptap styles in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@mantine/tiptap/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <MantineProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </MantineProvider>
   );
 }
